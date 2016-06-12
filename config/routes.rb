a22494397493f0ea95e9e4383e1e33e67056bede
kaylabutler <kaylabutler.violin@yahoo.com>
2016-06-12 14:39:53 -0700

    fixed bug in routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#landing_page'
 
-  resources :orders, only [:index, :show, :create, :destroy]
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
