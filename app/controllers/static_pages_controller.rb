7e23e5e40212601f0d3ab39ca8656fc427ad1361
kaylabutler <kaylabutler.violin@yahoo.com>
2016-07-14 17:38:18 -0700

    Enabled ActionMailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,14 @@ class StaticPagesController < ApplicationController
   	@products = Product.all
   	@featured_product = Product.first
   end
+
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(:from => @email,
+  		:to => 'kaylabutler.violin@yahoo.com',
+  		:subject => "A new contact form message from #{@name}",
+  		:body => @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/landing_page'
 
+  post 'static_pages/thank_you'
+
   resources :products
   get 'static_pages/about'
 
