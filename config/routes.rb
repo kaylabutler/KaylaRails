51b46a8fa56e90ae4dd6dd0c0fb0fb6075deb064
kaylabutler <kaylabutler.violin@yahoo.com>
2016-09-09 18:56:26 -0700

    Added a ratings system, and fixed the display of the Show Product pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  resources :products do
+    resources :comments
+  end
   resources :users
+  
   get 'static_pages/landing_page'
 
   post 'static_pages/thank_you'
 
-  resources :products
   get 'static_pages/about'
 
   get 'static_pages/contact'
