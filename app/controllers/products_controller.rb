48930180dab2de5bbc5a1f37d66bbd411a8b46af
kaylabutler <kaylabutler.violin@yahoo.com>
2016-09-13 22:31:11 -0700

    made pages look prettier

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,7 +17,7 @@ class ProductsController < ApplicationController
   # GET /products/1.json
   def show
     @comments = @product.comments.order("created_at DESC")
-    @comments = Comment.paginate(:page => params[:page], :per_page => 5)
+    @comments = Comment.paginate(:page => params[:page], :per_page => 3)
   end
 
   # GET /products/new
