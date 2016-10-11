f861c800b030c62ffc7ab417e994be1eab238962
kaylabutler <kaylabutler.violin@yahoo.com>
2016-10-11 13:01:46 -0700

    6.10 used AJAX to create comments remotely and render partial

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,4 @@
-$(document).on('ready page:load', function(){
+var refreshRating = function() {
 	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
 	$('.rated').raty({ path: '/assets',
 		readOnly: true,
@@ -6,6 +6,10 @@ $(document).on('ready page:load', function(){
 			return $(this).attr('data-score');
 		}
 	});
+};
+
+$(document).on('ready page:load ajaxSuccess', function() {
+	refreshRating();
 
 		$('.img-zoom').elevateZoom({
     zoomType: "lens",
@@ -13,6 +17,6 @@ $(document).on('ready page:load', function(){
     lensSize: 200,
 		lensFadeIn: 200,
 		lensFadeOut: 200
-  });
+	});
 	
 });
